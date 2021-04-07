6b2c05360aa2fd89b20b2959586e249b1a78f698
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-04-07 15:40:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+ /* Assignment A
+ const addTheWordCool = ["nice", "awesome", "tof"]
+  const add = addTheWordCool.push ("cool");
+
+console.log(addTheWordCool);
+*/
+
+ /* Assignment B
+ const fruits = ['appels', 'peren', 'citroenen'];
+
+console.log(fruits.length);
+*/
+
+ /* Assignment C
+const land = ["Belgie", "Nederland", "Luxemburg"]
+
+const selectBelgiumFromBenelux = land[0];
+
+console.log(selectBelgiumFromBenelux);
+*/
+
+/* Assignment D
+
+const dier = ["Haas", "Cavia", "Kip", "Schildpad"]
+const lastElementInArray = dier [dier.length - 1];
+console.log(lastElementInArray); 
+ */
+
+
+/* Assignment E
+const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+  
+
+  console.log(presidents.slice(1));
+  */
+
+/* Assignment F
+
+const stringsTogether = ['Winc', 'Academy', 'is', 'leuk', ';-}']
+console.log(stringsTogether.join(' '));
+*/
+
+/* Assignment F
+
+const array1 = [1,2,3]
+const array2 = [4,5,6]
+
+console.log (array1.concat (array2));
+*/
